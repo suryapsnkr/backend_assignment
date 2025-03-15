3c53993e57f757902e464a2b070131bb24e4ba03
Surya Prakash Sonkar <91277661+suryapsnkr@users.noreply.github.com>
2025-03-15 19:18:50 +0530

    Delete whatsappbot directory

--- whatsappbot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import send_whatsapp_message, whatsapp_webhook
-
-urlpatterns = [
-    path('send/', send_whatsapp_message, name="send_whatsapp_message"),
-    path('webhook/', whatsapp_webhook, name="whatsapp_webhook"),
-]
