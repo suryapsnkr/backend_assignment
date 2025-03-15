8279fc3bad129232b588fe546ce5d09fb7602327
Surya Prakash Sonkar <91277661+suryapsnkr@users.noreply.github.com>
2025-03-15 19:19:23 +0530

    urls.py

--- backend_assignment/urls.py
+++ backend_assignment/urls.py
@@ -7,7 +7,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-    path('whatsapp/', include('whatsappbot.urls')),
 ]
 
 
